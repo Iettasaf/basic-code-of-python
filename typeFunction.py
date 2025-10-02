ed6971feb7b4020d07be3a518565cbef363655b8
Iettasaf <tazulhossain1111@gmail.com>
2025-10-02 21:36:18 +0600

    Add files via upload

---
+++ typeFunction.py
@@ -0,0 +1,9 @@
+#type variable assigned using input() Function
+a = input("Enter the value of a: ")
+print(type(a))#it will give string
+b = int(input("Enter the value of b: "))
+print(type(b))#it will be integer
+c = float(input("Enter the value of b: "))
+print(type(c))#it will be float
+d = bool (input("Enter the value of b: "))
+print(type(d))#it will boolean
